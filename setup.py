6f40d344522c021f10ae18d9957e234eda6ebafc
Jiri Popelka <jpopelka@redhat.com>
2019-02-08 17:01:28 +0100

    setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name='zdravomil',
-    version='0.1.0',
-    packages=find_packages(exclude=['tests']),
-    url='https://github.com/user-cont/zdravomil'
-)
+setup()
